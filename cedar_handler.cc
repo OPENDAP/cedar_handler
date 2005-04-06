35f1cc45e7761dedce42c07417e9724b835898f0
Patrick West <pwest@opendap.org>
2005-04-06 20:14:24 +0000

    added initialization and termination calls

--- cedar_handler.cc
+++ cedar_handler.cc
@@ -28,23 +28,42 @@
  
 #include "config_cedar.h"
 
-static char not_used rcsid[]={"$Id: cedar_handler.cc,v 1.2 2004/12/15 17:44:12 pwest Exp $"};
+static char not_used rcsid[]={"$Id: cedar_handler.cc,v 1.3 2005/04/06 20:14:24 pwest Exp $"};
 
 #include "DODSCgi.h"
 #include "CedarFilter.h"
+#include "DODSGlobalIQ.h"
+#include "DODSException.h"
 
 int 
 main(int argc, char *argv[])
 {
+    try
+    {
+	putenv( "DODS_INI=/project/cedar/bin/apache_1.3.29/conf/opendap.ini" ) ;
+	DODSGlobalIQ::DODSGlobalInit( argc, argv ) ;
+    }
+    catch( DODSException &e )
+    {
+	cerr << "Error initializing application" << endl ;
+	cerr << "    " << e.get_error_description() << endl ;
+	return 1 ;
+    }
+
     CedarFilter df(argc, argv);
 
     DODSCgi d( "cedar", df ) ;
     d.execute_request() ;
 
+    DODSGlobalIQ::DODSGlobalQuit() ;
+
     return 0;
 }
 
 // $Log: cedar_handler.cc,v $
+// Revision 1.3  2005/04/06 20:14:24  pwest
+// added initialization and termination calls
+//
 // Revision 1.2  2004/12/15 17:44:12  pwest
 // added copyright, updated container persistence method look_for
 //
